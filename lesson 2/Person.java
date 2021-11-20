0ffec620df7bf8fad4624a080866e725d3653e10
ShonJava <f5df01d29806@gmail.com>
2021-11-20 15:29:53 +0500

    home work lesson 2

---
+++ lesson 2/Person.java
@@ -0,0 +1,25 @@
+public class Person {
+    
+    String male = "man";
+    String name = "Piter";
+    float height = 1.80f;
+    int age = 20;
+
+    void move() {
+        System.out.println("moving");
+    }
+    void sit() {
+        System.out.println("sit down");
+    }
+    void run() {
+        System.out.println("running");
+    }
+    String speak() {
+        System.out.println("speaking" + " ");
+        return "hello";
+    }
+    boolean learn() {
+        System.out.println("learning Java" + " ");
+        return true;
+    }
+}
---
+++ lesson 2/PersonTest.java
@@ -0,0 +1,11 @@
+public class PersonTest {
+    public static void main(String[] args) {
+    Person personOne = new Person();
+    System.out.println("Name of person " + personOne.name);
+    System.out.println("Male of person " + personOne.male);
+    System.out.println("Height of peroson " + personOne.height);
+    System.out.println("Age og person " + personOne.age);
+    System.out.print("Person have ");
+    personOne.run();
+    }
+}
