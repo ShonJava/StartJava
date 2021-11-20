551769af992112e3701365fc84f550c80f7e4f8e
ShonJava <f5df01d29806@gmail.com>
2021-11-20 16:02:34 +0500

    home work lesson 2 OOP

---
+++ lesson 2/Wolf.java
@@ -0,0 +1,22 @@
+public class Wolf {
+    String male = "man";
+    String nick = "Cucumber";
+    int weight = 30;
+    String color = "grey";
+
+    void run() {
+        System.out.println("running");
+    }
+    void sit() {
+        System.out.println("sitting");
+    }
+    void walk() {
+        System.out.println("walking");
+    }
+    void sing() {
+        System.out.println("singing");
+    }
+    void hunt() {
+        System.out.println("hunting");
+    }
+}
