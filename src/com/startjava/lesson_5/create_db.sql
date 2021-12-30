18aab80e80e76096a25b0f1036829ca04c7efb1b
ShonJava <f5df01d29806@gmail.com>
2021-12-30 21:44:36 +0500

    home work

---
+++ src/com/startjava/lesson_5/create_db.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE jaegers;
+
+CREATE TABLE Jaegers (
+	id SERIAL PRIMARY KEY,
+	modelName TEXT,
+	mark SMALLINT,
+	height REAL,
+	weight REAL,
+	status TEXT,
+	origin TEXT,
+	launch DATE,
+	kaijuKILL SMALLINT
+);
