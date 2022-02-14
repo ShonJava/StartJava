9480e618d7f0d521901b9ea054839ccf7e572b4a
ShonJava <f5df01d29806@gmail.com>
2022-02-14 19:35:32 +0500

    Home work Wolf and WolfTest incapsul

--- src/com/startjava/lesson_2/wolf/Wolf.java
+++ src/com/startjava/lesson_2/wolf/Wolf.java
@@ -1,17 +1,40 @@
 package com.startjava.lesson_2.wolf;
 
 public class Wolf {
-    String name;
-    int age;
-    String color;
+    private String name;
+    private int age;
+    private String color;
 
-    void walk() {
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        if(age > 8) {
+            System.out.println("error");
+        } else {
+            this.age = age;
+        }
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void walk() {
         System.out.println(color + " " + name + " walking");
     }
-    void run() {
+    public void run() {
         System.out.println(color + " " + name + " running");
     }
-    void sing() {
+    public void sing() {
         System.out.println(color + " " + name + " singing, age is " + age);
     }
 }
--- src/com/startjava/lesson_2/wolf/WolfTest.java
+++ src/com/startjava/lesson_2/wolf/WolfTest.java
@@ -3,11 +3,19 @@ package com.startjava.lesson_2.wolf;
 public class WolfTest {
     public static void main(String[] args) {
         Wolf wolf = new Wolf();
-        wolf.name = "Kija";
-        wolf.age = 3;
-        wolf.color = "white";
+        wolf.setName("Kija");
+        wolf.setAge(9);
+        wolf.setColor("white");
         wolf.run();
         wolf.walk();
         wolf.sing();
     }
 }
+/*    Модифицируйте класс Wolf
+        у всех полей класса напишите модификатор доступа private
+а у его методов — public
+для доступа к полям создайте геттеры и сеттеры
+        в сеттере поля age реализуйте проверку: если возраст волка > 8 лет, то выведите сообщение "Некорректный возраст". В противном случае сохраните значение в поле
+        Модифицируйте класс WolfTest
+        с помощью сеттеров присвойте полям экземпляра класса Wolf какие-то значения
+        считайте эти значения из полей с помощью геттеров и отобразите в консоли*/
